6f4d51c1e8330b06fd2fb1b012e0919354a363a4
Mark Standen <markstanden80@icloud.com>
2020-09-02 23:09:25 +0100

    wrote the bad code version of data analysis

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello World...")
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((line: string): string[] => {
+    return line.split(',');
+  });
+
+let manUnitedWins = 0;
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+console.log(`Man United win total: ${manUnitedWins}`);
